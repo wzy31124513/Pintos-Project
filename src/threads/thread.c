1f75a9a0021b7c57ee5e9ec5c86a14a8522488fd
Ziyang Wang <wangzy4@shanghaitech.edu.cn>
2018-10-28 18:48:36 +0800

    Update src/threads/thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -661,7 +661,8 @@ void thread_yield1 (struct thread* cur){
     
       old_level = intr_disable ();
       if (cur != idle_thread) {
-         list_insert_ordered (&ready_list, &cur->elem,cmp, NULL);
+        list_push_back (&ready_list, &cur->elem);
+        list_sort(&ready_list,cmp,NULL);
       }
       cur->status = THREAD_READY;
       schedule ();
