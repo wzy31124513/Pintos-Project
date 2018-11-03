72ac0d941ed7657b9ebb894e6f4e614e6282781f
Ziyang Wang <wangzy4@shanghaitech.edu.cn>
2018-11-03 22:37:33 +0800

    Update src/userprog/process.c

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -31,38 +31,19 @@ process_execute (const char *file_name)
 {
   char *fn_copy;
   tid_t tid;
-
-  /*
   char* name;
   char* p;
-  name=strtok_r((char*)file_name," ",&p);*/
+  name=strtok_r((char*)file_name," ",&p);
 
-  char *f_name;
-
-  
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
   if (fn_copy == NULL)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
-  char *save_ptr;
-  f_name = malloc(strlen(file_name)+1);
-  strlcpy (f_name, file_name, strlen(file_name)+1);
-  f_name = strtok_r (f_name," ",&save_ptr);
-  /* Create a new thread to execute FILE_NAME. */
-  //printf("%d\n", thread_current()->tid);
-  tid = thread_create (f_name, PRI_DEFAULT, start_process, fn_copy);
-
-  /* Make a copy of FILE_NAME.
-     Otherwise there's a race between the caller and load(). */
- /* fn_copy = palloc_get_page (0);
-  if (fn_copy == NULL)
-    return TID_ERROR;
-  strlcpy (fn_copy, file_name, PGSIZE);*/
 
   /* Create a new thread to execute FILE_NAME. */
-  //tid = thread_create (name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (name, PRI_DEFAULT, start_process, fn_copy);
 
 
 
@@ -288,7 +269,9 @@ load (const char *file_name, void (**eip) (void), void **esp)
   process_activate ();
 
   /* Open executable file. */
-  file = filesys_open (file_name);
+  char* p;
+  char* name=strtok_r(fild_name," ",&p);
+  file = filesys_open (name);
   if (file == NULL)
     {
       printf ("load: %s: open failed\n", file_name);
