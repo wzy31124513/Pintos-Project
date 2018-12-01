3b2d196c1ce59d9a1138ff2fe0a00b0371b86394
Ziyang Wang <wangzy4@shanghaitech.edu.cn>
2018-12-02 01:28:17 +0800

    Update process.c

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -469,31 +469,23 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
          and zero the final PAGE_ZERO_BYTES bytes. */
       size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
-
-      /* Get a page of memory. */
-      uint8_t *kpage = palloc_get_page (PAL_USER);
-      if (kpage == NULL)
-        return false;
-
-      /* Load this page. */
-      if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
-        {
-          palloc_free_page (kpage);
-          return false;
-        }
-      memset (kpage + page_read_bytes, 0, page_zero_bytes);
-
-      /* Add the page to the process's address space. */
-      if (!install_page (upage, kpage, writable))
-        {
-          palloc_free_page (kpage);
-          return false;
-        }
+      struct page* p=page_alloc(upage,writable);
+      if (p==NULL)
+      {
+      return false;
+      }
+      if (page_read_bytes>0)
+      {
+        p->file=file;
+        p->offset=ofs;
+        p->rw_bytes=page_read_bytes;
+      }
 
       /* Advance. */
       read_bytes -= page_read_bytes;
       zero_bytes -= page_zero_bytes;
       upage += PGSIZE;
+      ofs+=page_read_bytes;
     }
   return true;
 }
