393e32591126df6785cc5893f41e0723dfeb2ea0
Ziyang Wang <wangzy4@shanghaitech.edu.cn>
2018-12-01 20:59:15 +0800

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -94,7 +94,7 @@ thread_init (void)
   lock_init (&file_lock);
   list_init (&ready_list);
   list_init (&all_list);
-  frame_init();
+  //frame_init();
   /* Set up a thread structure for the running thread. */
   initial_thread = running_thread ();
   init_thread (initial_thread, "main", PRI_DEFAULT);
