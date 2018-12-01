30d95a534bd0543187f38e5a5a75f685c3e63472
Ziyang Wang <wangzy4@shanghaitech.edu.cn>
2018-12-02 07:47:08 +0800

    Update process.c

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -274,8 +274,7 @@ struct Elf32_Phdr
 #define PF_X 1          /* Executable. */
 #define PF_W 2          /* Writable. */
 #define PF_R 4          /* Readable. */
-
-static bool setup_stack (void **esp,const char* file_name);
+static bool setup_stack (const char *file_name, void **esp);
 static bool validate_segment (const struct Elf32_Phdr *, struct file *);
 static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
                           uint32_t read_bytes, uint32_t zero_bytes,
