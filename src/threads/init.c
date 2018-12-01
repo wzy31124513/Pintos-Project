dfaf009756a0fdb1bfd8e746b043c7c43c9afea9
Ziyang Wang <wangzy4@shanghaitech.edu.cn>
2018-12-01 21:45:08 +0800

    try

--- src/threads/init.c
+++ src/threads/init.c
@@ -128,8 +128,10 @@ main (void)
   locate_block_devices ();
   filesys_init (format_filesys);
 #endif
-  frame_init();
-  swap_init();
+
+  frame_init ();
+  swap_init ();
+
   printf ("Boot complete.\n");
   
   /* Run actions specified on kernel command line. */
