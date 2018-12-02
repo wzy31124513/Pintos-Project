33892559208f063939dabf8c7f700e9f37dd75a0
Ziyang Wang <wangzy4@shanghaitech.edu.cn>
2018-12-02 09:00:30 +0800

    Update page.h

--- src/vm/page.h
+++ src/vm/page.h
@@ -3,12 +3,10 @@
 
 #include <hash.h>
 #include "devices/block.h"
-#include "filesys/off_t.h"
 #include "threads/synch.h"
 
 
-struct page 
-{
+struct page {
   void* addr;
   struct frame* frame;
   struct thread* t;
@@ -30,8 +28,7 @@ bool page_lock(const void* addr,bool writable);
 void page_unlock(const void* addr);
 void page_exit (void);
 void page_deallocate (void *vaddr);
-unsigned page_hash_func (const struct hash_elem *e, void *aux UNUSED);
-bool less (const struct hash_elem *a,const struct hash_elem *b,void *aux UNUSED);
-void page_destructor(struct hash_elem* e,void* aux UNUSED);
+hash_hash_func page_hash_func;
+hash_less_func less;
 
 #endif
