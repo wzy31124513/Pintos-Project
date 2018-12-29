3d22274e881a55295bc075d39e9829835151a8bf
Ziyang Wang <wangzy4@shanghaitech.edu.cn>
2018-12-30 02:58:57 +0800

    ref

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,14 +1,7 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
-#include <list.h>
-void syscall_init (void);
 
-struct fds
-{
-    int fd;
-    struct file *f;
-    struct dir* dir;
-    struct list_elem elem;
-};
-void exit1 (int status);
+void syscall_init (void);
+void syscall_exit (void);
+
 #endif /* userprog/syscall.h */
