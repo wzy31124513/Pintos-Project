c76c94aadc79ca92198da2328555f8b8dc68a344
Ziyang Wang <wangzy4@shanghaitech.edu.cn>
2018-12-31 06:00:51 +0800

    Update init.c

--- src/threads/init.c
+++ src/threads/init.c
@@ -37,8 +37,6 @@
 #include "filesys/filesys.h"
 #include "filesys/fsutil.h"
 #endif
-#include "vm/frame.h"
-#include "vm/swap.h"
 
 /* Page directory with kernel mappings only. */
 uint32_t *init_page_dir;
